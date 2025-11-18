f330de8749dae6ea49a528a811dbd43b3922f7cd
Fariborz Jafarzadeh <103027029+fariborzj2@users.noreply.github.com>
2025-11-18 20:10:03 +0330

    Update cancel link to use PHP url function

--- views/users/edit.php
+++ views/users/edit.php
@@ -26,7 +26,7 @@
             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                 به‌روزرسانی
             </button>
-            <a href="/users" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+            <a href="<?php echo url('/users') ?>" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
                 انصراف
             </a>
         </div>
